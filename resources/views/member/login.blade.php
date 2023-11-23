8b1a1287dbbf18ec534826bcebd85fe80f615250
yjson <marilyn75@nate.com>
2023-11-23 18:00:33 +0900

    login/sign

--- resources/views/member/login.blade.php
+++ resources/views/member/login.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-xl-6">
                 <div class="breadcrumb_content">
-                    <h4 class="breadcrumb_title">Logın</h4>
+                    <h4 class="breadcrumb_title">로그인</h4>
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Logın</li>
@@ -20,41 +20,62 @@
 </section>
 
 <!-- Our LogIn Register -->
-<section class="our-log bgc-fa">
+<section class="our-log bgc-fa log_sign_pd">
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-lg-6 offset-lg-3">
-                <div class="login_form inner_page">
+                <div class="login_form inner_page login_form_w">
                     @include('include.messagebox')
                     <form action="{{ route('login') }}" method="POST">
                         @csrf
-                        <div class="heading">
-                            <h3 class="text-center">로그인</h3>
-                            <p class="text-center">계정이 없으신가요? <a class="text-thm" href="{{ route('register') }}">&nbsp;&nbsp;&nbsp;회원가입!</a></p>
-                        </div>
-                         <div class="form-group">
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email Address" value="@if (Session::has('email')) {{ Session::get('email') }} @elseif (isset($_COOKIE["email"])) {{ $_COOKIE["email"] }} @else {{ old('email') }} @endif">
+                        <div class="heading log_sing_intro">
+                            <h3>안녕하세요.<br><span class="mont">GYEMOIM INC.</span> 입니다.</h3>
+                            <p>회원 서비스 이용을 위해 로그인 해주세요.</p>
+                            <!-- <p class="">계정이 없으신가요? <a class="text-thm" href="{{ route('register') }}">&nbsp;&nbsp;&nbsp;회원가입!</a></p> -->
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password" value="@if (isset($_COOKIE["password"])){{ $_COOKIE["password"] }}@endif">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="이메일을 입력해주세요"
+                                value="@if (Session::has('email')) {{ Session::get('email') }} @elseif (isset($_COOKIE["
+                                email"])) {{ $_COOKIE["email"] }} @else {{ old('email') }} @endif">
                         </div>
-                        <div class="form-group custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="remember" name="remember" @if(isset($_COOKIE["email"])) checked @endif>
-                            <label class="custom-control-label" for="remember">Remember me</label>
-                            <a class="tdu btn-fpswd float-right" href="#">Forgot Password?</a>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password" name="password"
+                                placeholder="비밀번호를 입력해주세요" value="@if (isset($_COOKIE["
+                                password"])){{ $_COOKIE["password"] }}@endif">
                         </div>
-                        <button type="submit" class="btn btn-log btn-block btn-thm2">Login</button>
-                        <div class="divide">
-                            <span class="lf_divider">Or</span>
-                            <hr>
+                        <button type="submit" class="btn btn-log btn-block btn-thm2">로그인</button>
+                        <div class="form-group log_bot">
+                            <a class="tdu" href="#">이메일 찾기</a>
+                            <div class="log_bar"></div>
+                            <a class="tdu" href="#">비밀번호 찾기</a>
+                            <div class="log_bar"></div>
+                            <a class="tdu" href="{{ route('register') }}">회원가입</a>
                         </div>
-                        <div class="row mt40">
+                        <!-- <div class="row mt40">
                             <div class="col-lg">
-                                <a href="{{ route('social.login','naver') }}" class="btn btn-block color-white bgc-naver mb0"><i class="fa fa-facebook float-left mt5"></i> Naver</a>
+                                <a href="{{ route('social.login','naver') }}"
+                                    class="btn btn-block color-white bgc-naver mb0"><i
+                                        class="fa fa-facebook float-left mt5"></i> Naver</a>
                             </div>
                             <div class="col-lg">
-                                <a href="{{ route('social.login','kakao') }}" class="btn btn2 btn-block color-black bgc-kakao mb0"><i class="fa fa-google float-left mt5"></i> Kakao</a>
+                                <a href="{{ route('social.login','kakao') }}"
+                                    class="btn btn2 btn-block color-black bgc-kakao mb0"><i
+                                        class="fa fa-google float-left mt5"></i> Kakao</a>
                             </div>
+                        </div> -->
+                        <div class="social_btn">
+                            <a href="{{ route('social.login','naver') }}">
+                                <div class="sns sns_n">
+                                    <img src="images/sns/naver.png" alt="">
+                                    <p>네이버로 로그인</p>
+                                </div>
+                            </a>
+                            <a href="{{ route('social.login','kakao') }}">
+                                <div class="sns sns_k">
+                                    <img src="images/sns/kakao.png" alt="">
+                                    <p><span>Kakao</span>로 로그인</p>
+                                </div>
+                            </a>
                         </div>
                     </form>
                 </div>
--- resources/views/member/register.blade.php
+++ resources/views/member/register.blade.php
@@ -21,14 +21,14 @@
 
 <!-- Our LogIn Register -->
 
-<section class="our-log-reg bgc-fa">
+<section class="our-log-reg bgc-fa log_sign_pd">
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-lg-6 offset-lg-3">
-                <div class="sign_up_form inner_page">
-                    <div class="heading">
-                        <h3 class="text-center">회원가입 정보를 입력하세요.</h3>
-                        <p class="text-center">이미 계정이 있습니까?<a class="text-thm" href="{{ route('login') }}"> &nbsp;&nbsp;&nbsp; Login</a></p>
+                <div class="sign_up_form inner_page sign_up_form_w">
+                    <div class="heading log_sing_intro">
+                        <h3>회원가입</h3>
+                        <p>계정이 있으신가요?<a class="text-thm" href="{{ route('login') }}">로그인</a></p>
                     </div>
                     <div class="details">
                         @include('include.messagebox')
@@ -52,18 +52,6 @@
                                 <label class="custom-control-label" for="exampleCheck3">Want to become an instructor?</label>
                             </div> --}}
                             <button type="submit" class="btn btn-log btn-block btn-thm2">가입하기</button>
-                            <div class="divide">
-                                <span class="lf_divider">Or</span>
-                                <hr>
-                            </div>
-                            <div class="row mt40">
-                                <div class="col-lg">
-                                    <button type="submit" class="btn btn-block color-white bgc-fb mb0"><i class="fa fa-facebook float-left mt5"></i> Facebook</button>
-                                </div>
-                                <div class="col-lg">
-                                    <button type="submit" class="btn btn2 btn-block color-white bgc-gogle mb0"><i class="fa fa-google float-left mt5"></i> Google</button>
-                                </div>
-                            </div>
                         </form>
                     </div>
                 </div>
