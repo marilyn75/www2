624adb743d9ae71899c472f0d029c3013440bf45
yjson <marilyn75@nate.com>
2023-11-28 16:28:09 +0900

    git commit -m 상담팝업

--- resources/views/include/page-permission.blade.php
+++ resources/views/include/page-permission.blade.php
@@ -1,21 +1,24 @@
 <div class="row">
     <div class="col-lg-10 offset-lg-1 text-center">
         <div class="error_page footer_apps_widget">
-            <img class="img-fluid" src="{{ asset('images/Common/permission.png') }}" alt="error.png">
-            <div class="erro_code"><h1>권한이 없습니다.</h1></div>
-            <p>
+            <img class="img-fluid" src="{{ asset('images/Common/stop_login.png') }}" alt="error.png">
+            <div class="erro_code"><h3>로그인 후 다양한<br>혜택을 만나보세요!</h3></div>
+            <!-- <p>
                 @auth
                     접근 권한이 없습니다.
                 @else
                     로그인 후 이용하세요.
                 @endauth
-            </p>
-            <form class="form-inline mailchimp_form">
+            </p> -->
+            <!-- <form class="form-inline mailchimp_form">
                 <label class="sr-only" for="inlineFormInputName">Name</label>
                 <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName" placeholder="Search">
                 <button type="submit" class="btn btn-primary mb-2"><span class="flaticon-magnifying-glass"></span></button>
-            </form>
+            </form> -->
         </div>
-        <a class="btn btn_error btn-thm" href="index.html">Back To Home</a>
+        <div class="error_page_btns">
+            <a class="btn btn_error btn-thm " href="index.html">로그인</a>
+            <a class="btn btn_error btn-thm" href="index.html">닫기</a>
+        </div>
     </div>
 </div>
