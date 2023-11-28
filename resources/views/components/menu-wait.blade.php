4665998a78ad81e9f0f6340efe308137ef1d8ff1
yjson <marilyn75@nate.com>
2023-11-28 10:15:18 +0900

    준비중입니다

--- resources/views/components/menu-wait.blade.php
+++ resources/views/components/menu-wait.blade.php
@@ -1,3 +1,9 @@
 <div class="w-100">
-    준비중 입니다.
+    <div class="container container_w">
+        <section class="row align-items-center">
+            <h2 class="col-lg-4 service_r">서비스 준비중입니다.<br>곧 찾아뵙겠습니다!</h2>
+            <h2 class="col-lg-4 service_r mobile_serv">서비스 준비중입니다. 곧 찾아뵙겠습니다!</h2>
+            <div class="col-lg-8 service_img"><img src="/images/example.png" alt=""></div>
+        </section>
+    </div>
 </div>
