89bb65ad561dfccc51c5c6ee6895bc91bf479564
ckkim <marilyn75@nate.com>
2023-11-29 17:28:35 +0900

    매물상세 지도 마커이미지 표시

--- resources/views/components/kko_map.blade.php
+++ resources/views/components/kko_map.blade.php
@@ -22,16 +22,34 @@
     // 지도를 생성한다 
     var map = new kakao.maps.Map(mapContainer, mapOption); 
 
+    // 이미지 마커
+    var imageSrc = '/images/map/map_mark.png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(65, 65), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(32.5, 32.5)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+        markerPosition = new kakao.maps.LatLng({{ $y }}, {{ $x }}); // 마커가 표시될 위치입니다
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition, 
+        image: markerImage // 마커이미지 설정 
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);  
+      
+
     // 지도에 원을 표시한다
     var circle = new kakao.maps.Circle({
         map: map, // 원을 표시할 지도 객체
         center : new kakao.maps.LatLng({{ $y }}, {{ $x }}), // 지도의 중심 좌표
-        radius : 200, // 원의 반지름 (단위 : m)
-        fillColor: '#d1c5b2', // 채움 색
+        radius : 300, // 원의 반지름 (단위 : m)
+        fillColor: 'rgba(56, 95, 141, 0.44)', // 채움 색
         fillOpacity: 0.9, // 채움 불투명도
         strokeWeight: 3, // 선의 두께
-        strokeColor: '#d1c5b2', // 선 색
-        strokeOpacity: 0.9, // 선 투명도 
+        strokeColor: 'rgba(56, 95, 141, 0.44)', // 선 색
+        strokeOpacity: 0.1, // 선 투명도 
         strokeStyle: 'solid' // 선 스타일
     });	
     // 마우스 휠과 모바일 터치를 이용한 지도 확대, 축소를 막는다
@@ -42,7 +60,6 @@
 
     // 마커를 생성하고 지도위에 표시하는 함수입니다
     function addMarker(position) {
-        console.log('마커추가');
         // 마커를 생성합니다
         var marker = new kakao.maps.Marker({
             position: position
