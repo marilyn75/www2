96df8381a55fd0e9a18e88f9aee7303e0e0ce68f
yjson <marilyn75@nate.com>
2023-12-08 16:01:10 +0900

    watermark

--- resources/views/components/module-sale-intranet-recommend.blade.php
+++ resources/views/components/module-sale-intranet-recommend.blade.php
@@ -4,34 +4,6 @@
     <input type="hidden" name="mode" value="{{ $_REQUEST['mode'] }}">
 <section class="our-listing bgc-f7 pb30-991">
     <div class="container container_w">
-        <section class="row top_cate pt-0">
-            <div class="cate_wrap">
-                <div class="cate_bg">
-                    <img src="/images/top_cate/busanilbo.png" alt="">
-                </div>
-                <p>부산일보</p>
-            </div>
-            <div class="cate_wrap">
-                <div class="cate_bg"></div>
-                <p>부산일보</p>
-            </div>
-            <div class="cate_wrap">
-                <div class="cate_bg"></div>
-                <p>부산일보</p>
-            </div>
-            <div class="cate_wrap">
-                <div class="cate_bg"></div>
-                <p>부산일보</p>
-            </div>
-            <div class="cate_wrap">
-                <div class="cate_bg"></div>
-                <p>부산일보</p>
-            </div>
-            <div class="cate_wrap">
-                <div class="cate_bg"></div>
-                <p>부산일보</p>
-            </div>
-        </section>
         <div class="row">
             <div class="col-lg-12">
                 <div class="row">
