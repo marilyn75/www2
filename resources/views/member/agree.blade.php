bc2839a17b07f839028619becf2cb2f82074dee8
yjson <marilyn75@nate.com>
2023-12-28 17:07:12 +0900

    인증번호

--- resources/views/member/agree.blade.php
+++ resources/views/member/agree.blade.php
@@ -10,18 +10,33 @@
             <div class="col-sm-12 col-lg-6 offset-lg-3">
                 <div class="sign_up_form inner_page sign_up_form_w">
                     <div class="heading log_sing_intro">
-                        <h3>약관 동의</h3>
-                        <p>환영합니다! 계모임 서비스 이용약관에 동의해주세요.</a></p>
+                        <h3>약관동의</h3>
                     </div>
                     <div class="details">
                         @include('include.messagebox')
                         <form action="{{ url(route('register')) }}" method="POST">
                             @csrf
                             <input type="hidden" name="mode" value="form">
-                            <div class="form-group custom-control custom-checkbox">
+                            
+                            {{-- 전체동의 --}}
+                            <div class="form-group custom-control custom-checkbox agree_bx">
                                 <input type="checkbox" class="custom-control-input" id="exampleCheck3" name="agree" value="1">
-                                <label class="custom-control-label" for="exampleCheck3">모두 동의합니다.</label>
+                                <label class="custom-control-label" for="exampleCheck3">전체동의</label>
                             </div>
+
+                            {{-- 서비스 이용약관 --}}
+                            <div class="form-group custom-control custom-checkbox agree_bx">
+                                <input type="checkbox" class="custom-control-input" id="exampleCheck3" name="agree" value="1">
+                                <label class="custom-control-label" for="exampleCheck3">서비스 이용약관에 동의합니다.</label>
+                            </div>
+
+                            {{-- 개인정보처리방침 --}}
+                            <div class="form-group custom-control custom-checkbox agree_bx">
+                                <input type="checkbox" class="custom-control-input" id="exampleCheck3" name="agree" value="1">
+                                <label class="custom-control-label" for="exampleCheck3">개인정보처리방침에 동의합니다.</label>
+                            </div>
+
+                            
                             <button type="submit" class="btn btn-log btn-block btn-thm2">동의하기</button>
                         </form>
                     </div>
--- resources/views/member/register.blade.php
+++ resources/views/member/register.blade.php
@@ -31,6 +31,13 @@
                             <div class="form-group">
                                 <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="비밀번호 확인" >
                             </div>
+                            <div class="form-group phone_num_chk">
+                                <input type="number" class="form-control" id="phnumber" name="phnumber" placeholder="휴대폰 번호를 입력하세요" >
+                                <button>인증</button>
+                            </div>
+                            <div class="form-group">
+                                <input type="number" class="form-control" id="confirm" name="confirm" placeholder="인증번호를 입력하세요" >
+                            </div>
                             {{-- <div class="form-group custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" id="exampleCheck3">
                                 <label class="custom-control-label" for="exampleCheck3">Want to become an instructor?</label>
