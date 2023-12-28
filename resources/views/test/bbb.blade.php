23cf80ceb42f00fb325525e01ded9da0aaedffb9
yjson <marilyn75@nate.com>
2023-12-28 16:06:03 +0900

    about

---
+++ resources/views/test/bbb.blade.php
@@ -0,0 +1,161 @@
+@extends('layout.layout')
+
+@section('content')
+
+
+ <!-- Gyemoim 소개 -->
+ <div class="container container_w">
+    <section class="intro_top pb100">
+        <h2 class="intro_tit mont">GYEMOIM INC.</h2>
+        <h2 class="intro_tit">부동산은 계약을 통해 완성됩니다.<br>계약을 필요로 하는 사람들의 모임,</h2>
+        <p class="intro_txt">모두의 선택을 더 나은 방향으로 이끄는 곳입니다.</p>
+        <p class="intro_txt"><br>부동산 시세를 이해하는 지식을 갖추었나요?<br>이제 우리의 경험이 빚어낸 최고의 매물과 함께 지혜로운 선택을 할 차례입니다.<br>부동산
+            투자의 안전한 시작 <span class="mont">GYEMOIM INC.&nbsp;</span>과 함께하세요.</p>
+    </section>
+</div>
+
+
+<!-- 핵심가치 -->
+<section class="core">
+    <div class="container container_w">
+        <h2><span class="mont">GYEMOIM INC.</span> 의 핵심가치</h2>
+        <div class="intro_boxs">
+            <div class="intro_bx">
+                <h2 class="mont">EXPERTISE</h2>
+                <p>오랜경험과 풍부한 전문지식</p>
+            </div>
+            <div class="intro_bx">
+                <h2 class="mont">RELIABILITY</h2>
+                <p>안정적이고 믿을수 있는 파트너</p>
+            </div>
+            <div class="intro_bx">
+                <h2 class="mont">SERVICE</h2>
+                <p>맞춤형 서비스</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<div class="container container_w">
+
+    <!-- collaboration -->
+    <section class="about_top pt100 pb100">
+        <div class="bg_txt">
+            <h2 class="mont">COLLABORATION</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 pt80">
+                <div class="about_us">
+                    <h2 class="mt0"><span class="mont">GYEMOIM INC. x</span> (주)부동산중개법인개벽</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center mb100">
+            <div class="col-md-5">
+                <div class="about_content">
+                    <img src="images/about/intro_collabo.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-7">
+                <p class="intro_txt mt_txt">드디어 계모임과 개벽이 만났습니다.</p>
+                <p class="intro_txt"><br>최상의 중개활동 환경 구축을 목표로 하는 계모임과 부울경 최고의 부동산 중개인들이 한곳에 모였습니다.<br><br>우리의
+                    Collaboration 은 부동산 계약을 필요로 하는 사람들에게 새로운 차원의 부동산 경험을 제공합니다.</p>
+            </div>
+        </div>
+        <div class="row mt100">
+            <div class="col-md-4">
+                <div class="why_chose_us style2">
+                    <div class="icon">
+                        <img src="images/about/about_build.png" alt="">
+                    </div>
+                    <div class="details">
+                        <h4>등록된 매물 개수</h4>
+                        <p><span class="mont">80,123</span> 개</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="why_chose_us style2">
+                    <div class="icon">
+                        <img src="images/about/about_employ.png" alt="">
+                    </div>
+                    <div class="details">
+                        <h4>부동산전문가</h4>
+                        <p><span class="mont">87</span> 명</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="why_chose_us style2">
+                    <div class="icon">
+                        <img src="images/about/about_money.png" alt="">
+                    </div>
+                    <div class="details">
+                        <h4>계약 완료</h4>
+                        <p><span class="mont">2,300</span>&nbsp;건</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- vision -->
+    <section class="about_top pt100 pb100">
+        <div class="bg_txt">
+            <h2 class="mont">OUR VISION</h2>
+        </div>
+        <div class="col-lg-12 pt80">
+            <div class="about_us">
+                <h2 class="mt0">계모임의 미래 비전</h2>
+                <p>계모임의 매물을 관리하는 기술력과 개벽의 전문성이 만난다면,<br>
+                    부울경 부동산 거래는 더욱 효율적이고 투명하며 안전한 과정으로 진화할 것입니다.</p>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- vision -->
+    <section class="about_top pt100 pb100">
+        <div class="bg_txt">
+            <h2 class="mont">ADVANTAGE</h2>
+        </div>
+        <div class="col-lg-12 pt80">
+            <div class="about_us">
+                <h2 class="mt0">계모임 <span class="mont">x</span> 개벽의 강점</h2>
+                <p>
+                    시장에 나온 부동산 매물은 가격과 조건을 가지고 있습니다.<br>
+                    실거래가 데이터로 인해서 가격을 이해하셨나요?<br>
+                    서로의 조건에 따라 가격 조율이 가능한 것이 부동산입니다. <br><br>
+
+                    계모임의 기술적 역량과 혁신적인 접근법으로<br>
+                    더욱 효율적이고 원활한 최상의 중개환경을 구축하였습니다.<br><br>
+
+                    각 매물 유형별로 구성된 개벽의 전문 중개인들은 시장 동향을 선도하며<br>
+                    고객들에게 최상의 조언을 담은 최고의 중개서비스를 제공합니다.<br><br>
+                </p>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- contact -->
+    <section class="about_top pt100 pb100">
+        <div class="bg_txt">
+            <h2 class="mont">CONTACT</h2>
+        </div>
+        <div class="col-lg-12 pt80">
+            <div class="about_us">
+                <h2 class="mt0">문의하기</h2>
+            </div>
+            <img src="images/about/contact_map.png" alt="">
+            <div class="contact_us">
+                <p>Tel · E-mail: 051-791-8888 · info@email.com</p>
+                <p>Address : 부산 연제구 중앙대로 1091</p>
+            </div>
+        </div>
+    </section>
+
+</div>
+@endsection
--- routes/test.php
+++ routes/test.php
@@ -4,4 +4,8 @@ Route::get('/test2', function(){
     return view('test.aaa');
 });
 
+Route::get('/test3', function(){
+    return view('test.bbb');
+});
+
 ?>
