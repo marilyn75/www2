9107349517fc7c7af7c19b50392fc7d3c36f0e55
yjson <marilyn75@nate.com>
2023-12-29 14:05:36 +0900

    실시간 상담 버튼 반응형

--- resources/views/components/chat-icon.blade.php
+++ resources/views/components/chat-icon.blade.php
@@ -3,6 +3,7 @@
 	<button onclick="openWindow('{{ route('chat') }}',600, 900, 'chat');return false;">
     <p>무엇을 <br> 도와드릴까요?</p>
     <p class="mont click_chat">click</p>
+    <i class="ri-message-3-line"></i>
     </button>
     @if ($cnt>0)
     <div class="notification-sticker">{{ $cnt }}</div>
