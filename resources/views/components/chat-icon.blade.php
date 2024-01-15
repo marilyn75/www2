7d33e3f6f309fc39f60ad25e2cc515409e48d8c7
ckkim <marilyn75@nate.com>
2024-01-15 10:16:34 +0900

    문의채팅  로그인 안한경우 모달창 띄우기

--- resources/views/components/chat-icon.blade.php
+++ resources/views/components/chat-icon.blade.php
@@ -1,6 +1,12 @@
 <div class="chat-icon">
 	<!-- <img src="{{ asset('images/chat/live-chat.png') }}" alt="상담채팅" onclick="openWindow('{{ route('chat') }}',600, 900, 'chat');return false;"> -->
-	<button onclick="openWindow('{{ route('chat') }}',600, 900, 'chat');return false;">
+	<button 
+    @auth
+    onclick="openWindow('{{ route('chat') }}',600, 900, 'chat');return false;"
+    @else
+    data-toggle="modal" data-target="#logalertModal" onclick="return false;"
+    @endauth
+    >
     <p>무엇을 <br> 도와드릴까요?</p>
     <p class="mont click_chat">click</p>
     <i class="ri-message-3-line"></i>
