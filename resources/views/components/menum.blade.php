88c1980cc6b0614a07f522f6cb30832d14318cfd
ckkim <marilyn75@nate.com>
2024-01-15 12:00:27 +0900

    모바일 메뉴 상단  로그인 전, 후 처리

--- resources/views/components/menum.blade.php
+++ resources/views/components/menum.blade.php
@@ -1,17 +1,18 @@
 <ul>
     <li class="mmenu_logo"><img src="/images/mmenu_logo.png" alt=""></li>
-
-    {{-- 로그인 전 --}}
-    {{-- <li class="cl_btn"><a class="btn btn-block btn-lg btn-thm" href="#">로그인</a></li> --}}
-    
+    @auth
     {{-- 로그인 후 --}}
     <div class="login_ham">
         {{-- 프로필 이미지 --}}
-        <img src="http://gyemoim.co.kr/images/user-placeholder.png" alt="">
-        <p>계모임</p>
+        <img src="{{ showProfileImage() }}" alt="">
+        <p>{{ Auth::user()->name }}</p>
         {{-- 마이페이지 링크로 이동 --}}
-        <button class="btn mont">MY<i class="ri-arrow-right-s-fill"></i></button>
+        <button class="btn mont"><a href="{{ route('mypage') }}">MY<i class="ri-arrow-right-s-fill"></i></a></button>
     </div>
+    @else 
+    {{-- 로그인 전 --}}
+    <li class="cl_btn"><a class="btn btn-block btn-lg btn-thm" href="{{ route('login') }}">로그인</a></li>
+    @endauth
     
     @foreach ($menus as $menu)
     <x-menum-item :menu="$menu" />
