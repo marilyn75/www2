2d8ba1eafb8578da394900f491e0898b032083b1
yjson <marilyn75@nate.com>
2024-03-07 14:11:37 +0900

    pdfmodal

--- resources/views/modal/pdfviewer.blade.php
+++ resources/views/modal/pdfviewer.blade.php
@@ -3,13 +3,21 @@
     $file = $path . $data['saNo'] . '/' . $data['fn'];
 @endphp
 
-<div class="modal fade login_modal logmod aucmod" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel"
+<div class="modal fade login_modal logmod aucpdfmod" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel"
     aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
 
             <div class="modal-scroll">
                 <iframe src="http://apidata.localhost:8080/PDFViewer/full/viewer.html?file={{ $file }}" frameborder="0" height="100%"></iframe>
+            </div>
+
+            <div class="mod-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">
+                        <img src="../images/modal/close_btn.png" alt="">
+                    </span>
+                </button>
             </div>
 
         </div>
