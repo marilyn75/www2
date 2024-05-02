3269adf92013df89ec40c97e394aa6be4b618949
ckkim <marilyn75@nate.com>
2024-05-02 15:03:50 +0900

    메인 슬라이드 스크립트

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -111,7 +111,8 @@
     </div>
 </form>
 
-  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+  <script src="/js/swiper-element-bundle.min.js">
+</script>
 
 <!-- Feature Properties -->
 <section id="feature-property" class="feature-property bgc-f7">
